d4a5394e5473ccbf0b4d139c8bde3cad2a33d911
ho-ong8 <honghj8959@gmail.com>
2024-08-26 22:36:25 +0900

    Add programmers code (Java)

---
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 특정 문자 제거하기/[Lv.0] 특정 문자 제거하기.java
@@ -0,0 +1,6 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        // replace(target, replacement): 문자열을 다른 문자열로 치환
+        return my_string.replace(letter, "");
+    }
+}
