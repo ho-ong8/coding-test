c2d6c488355c9a3fde60a6929b2b38636d376c0b
ho-ong8 <honghj8959@gmail.com>
2024-09-16 05:07:34 +0900

    Add programmers code (Java)

---
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 합성수 찾기/[Lv.0] 합성수 찾기_00.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0; // 합성수의 개수
+
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0; // 약수의 개수
+
+            // 약수 구하기
+            for (int j = 1; j <= i; j++) {
+                // i를 j로 나눴을 때, 나머지가 0일 경우
+                if (i % j == 0) {
+                    cnt += 1;
+                }
+            }
+
+            // 합성수 구하기
+            // 약수의 개수가 3개 이상일 경우
+            if (cnt >= 3) {
+                answer += 1;
+            }
+        }
+
+        return answer;
+    }
+}
---
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 합성수 찾기/[Lv.0] 합성수 찾기_01.java
@@ -0,0 +1,13 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int n) {
+        // 기본형 스트림 생성 -> 원소 추출 (기본형 스트림 생성 -> 원소 추출 -> 개수 계산) -> 개수 계산
+        // rangeClosed(start, end): start(포함)부터 end(포함)까지 범위 내 정수 생성
+        // filter(): 조건에 해당하는 원소를 추출하여 반환
+        // count(): 개수 반환
+        return (int) IntStream.rangeClosed(1, n).filter(
+            i -> (int) IntStream.rangeClosed(1, i).filter(j -> i % j == 0).count() >= 3 // 약수
+        ).count(); // 합성수
+    }
+}
