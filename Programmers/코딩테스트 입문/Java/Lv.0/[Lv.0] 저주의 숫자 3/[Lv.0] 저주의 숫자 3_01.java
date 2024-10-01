2e5a42c1056a58cd3b7e72194ee7645b77b381b7
ho-ong8 <honghj8959@gmail.com>
2024-10-02 06:17:15 +0900

    Add programmers code (Java)

---
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 저주의 숫자 3/[Lv.0] 저주의 숫자 3_01.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0; // 3x 마을의 숫자
+
+        for (int i = 1; i <= n; i++) {
+            answer++;
+
+            // String.valueOf(): 문자열 변환
+            // contains(): 문자열 포함 여부 확인
+            // answer를 3으로 나눴을 때, 나머지가 0일 경우, "3"이 포함되어 있을 경우
+            if (answer % 3 == 0 || String.valueOf(answer).contains("3")) {
+                i--;
+            }
+        }
+
+        return answer;
+    }
+}
