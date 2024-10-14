0121298c405dd47d3d67748080f9e3d1c8e7f5ef
ho-ong8 <honghj8959@gmail.com>
2024-10-14 20:51:20 +0900

    Add programmers code (Java)

---
+++ Programmers/코딩 기초 트레이닝/Java/문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        // print(): 출력 (줄 바꿈 X)
+        // println(): 출력 (줄 바꿈 O)
+        // printf(서식, 내용): 출력 (서식 지정, 줄 바꿈 X)
+        System.out.println(a);
+    }
+}
