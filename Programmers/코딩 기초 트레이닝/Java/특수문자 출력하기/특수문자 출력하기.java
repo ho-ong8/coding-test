19bddfe435b6ddfcb62f137610383b971c835898
ho-ong8 <honghj8959@gmail.com>
2024-10-17 02:15:44 +0900

    Add programmers code (Java)

---
+++ Programmers/코딩 기초 트레이닝/Java/특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // \t: 탭
+        // \n: 줄 바꿈
+        // \': 작은 따옴표
+        // \": 큰 따옴표
+        // \\: 역 슬래시
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
