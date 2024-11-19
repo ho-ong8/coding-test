3258fcccc1d359b7fdb99e86b0febfdbdcdc6637
ho-ong8 <honghj8959@gmail.com>
2024-11-19 14:54:58 +0900

    Add programmers code (Java)

---
+++ Programmers/코딩 기초 트레이닝/Java/배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        // list 생성
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            // isEmpty(): 문자열의 길이가 0일 경우 true 반환
+            // 빈 리스트일 경우
+            if (list.isEmpty()) {
+                // add(): 원소 추가
+                list.add(arr[i]);
+            // 리스트의 마지막 원소가 작을 경우
+            } else if (list.get(list.size() - 1) < arr[i]) {
+                list.add(arr[i]);
+            } else {
+                // remove(): 원소 삭제
+                // size(): 컬렉션 타입 길이 반환
+                list.remove(list.size() - 1);
+                i--;
+            }
+        }
+
+        int[] stk = new int[list.size()];
+        int cnt = 0; // 인덱스
+
+        for (int i = 0; i < list.size(); i++) {
+            stk[cnt++] = list.get(i);
+        }
+
+        return stk;
+    }
+}
