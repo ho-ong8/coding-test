f12e4c70bc28e5d389511ef093f33ffe395eca9c
ho-ong8 <honghj8959@gmail.com>
2024-11-27 02:09:18 +0900

    Add programmers code (Java)

---
+++ Programmers/코딩 기초 트레이닝/Java/문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,6 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        // substring(startIndex, endIndex): startIndex(포함)부터 endIndex(불포함)까지 범위 내 문자열 자르기
+        return my_string.substring(0, n);
+    }
+}
