0ca06be2d36c13302995c66b1fe8e96d0c270ad6
ho-ong8 <honghj8959@gmail.com>
2024-12-06 09:29:52 +0900

    Add programmers code (Java)

--- Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 숫자 찾기/[Lv.0] 숫자 찾기_01.java
+++ Programmers/코딩테스트 입문/Java/Lv.0/[Lv.0] 숫자 찾기/[Lv.0] 숫자 찾기_01.java
@@ -5,7 +5,7 @@ class Solution {
         return ("-" + num).indexOf(String.valueOf(k));
 
         // "-" + num 하는 이유?
-        // num 인덱스 1부터 시작
+        // 인덱스 1부터 시작
         // 문자열 변환 => 문자열 + 정수 = 문자열
     }
 }
