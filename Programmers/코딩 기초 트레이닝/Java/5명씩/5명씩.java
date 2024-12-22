9817fdf0e3e200293ab46e47f93cddbc658542fc
ho-ong8 <honghj8959@gmail.com>
2024-12-22 13:23:07 +0900

    Add programmers code (Java)

---
+++ Programmers/코딩 기초 트레이닝/Java/5명씩/5명씩.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String[] solution(String[] names) {
+        String[] answer = new String[(names.length + 4) / 5];
+        int cnt = 0; // 인덱스
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = names[cnt];
+            cnt += 5;
+        }
+
+        return answer;
+    }
+}
